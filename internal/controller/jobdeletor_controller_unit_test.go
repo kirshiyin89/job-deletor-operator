40b52428ef8fcef2ac038e5c041b85447c608f10
Kirshi Yin <info@curiousdevscorner.org>
2026-06-07 21:20:23 +0200

    Revert "Add event recording import to jobdeletor tests"
    This reverts commit a7b035ff81330f0f84ad8ffc043690c6c78fa952.

--- internal/controller/jobdeletor_controller_unit_test.go
+++ internal/controller/jobdeletor_controller_unit_test.go
@@ -10,7 +10,6 @@ import (
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/record"
 	"k8s.io/apimachinery/pkg/runtime"
 
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
